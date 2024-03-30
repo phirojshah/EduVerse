850fd86eb5635de217e1a2b401886c658328dd71
phirojshah <aditya8877123@gmail.com>
2024-03-31 00:13:37 +0545

    Updated

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-const express = require('express')
-require('./config/database')
-const dotenv = require('dotenv');
-const { connect } = require('./config/database');
-dotenv.config();
+const express = require("express");
+const database = require("./config/database");
+const { data } = require("autoprefixer");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+database.connect();
 
-const app = express()
-const port = process.env.PORT || 5000
-connect();
-app.listen(port, () => {
-  console.log(`EduVerse Backend listening on port http://localhost:${port}`)
-})
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`EduVerse Backend listening on port http://localhost:${PORT}`);
+});
